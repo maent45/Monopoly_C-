a39dc913d1b01a83d7788157fffa5a02fa7b735b
Maen Terawasi <maent45@gmail.com>
2015-09-20 13:46:52 +1200

    created Board class
    started the Board class which handles main part of the program and
    determines which position player has landed in, still need to implement
    more methods to it.

---
+++ Monopoly/Board.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    // This is class for singleton Board that has properties and traders on it.
+    public class Board
+    {
+        //provide a static instance of this class to create singleton
+        static Board board;
+        private ArrayList properties;
+        private ArrayList players;
+        int SQUARES = 40;
+
+        //method to access Singleton
+        public static Board access()
+        {
+            if (board == null)
+                board = new Board();
+            return board;
+        }
+        public Board()
+        {
+            properties = new ArrayList(this.getSquares());
+            players = new ArrayList();
+        }
+        public int getSquares()
+        {
+            return this.SQUARES;
+        }
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        //method to add a player --still need to create the Player class
+        /*public void addPlayer(Player player)
+        {
+            players.Add(player);
+        }*/
+
+        //method to add property --still need to create the Property class
+        /*public void addProperty(Property property)
+        {
+            this.properties.Add(property);
+        }*/
+
+        //method to return amount of players
+        public int getPlayerCount()
+        {
+            return players.Count;
+        }
+    }
+}
