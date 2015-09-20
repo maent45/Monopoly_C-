e8a05bc89d8562ae94c817ecab21aa700111d92d
Maen Terawasi <maent45@gmail.com>
2015-09-20 13:29:51 +1200

    Initial Push - created Die and Card class
    created die class to show dice roll, and card class.

---
+++ Monopoly/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    //This class represents the cards such as 'Get out of Jail', 'Free', etc...
+    public class Card
+    {
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Monopoly/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    //This is a die class that will generate random numbers between 1-6 which represents a dice roll.
+    class Die
+    {
+        //create new instance of Randon
+        private static Random numberGenerator = new Random();
+        private int numberRolled;
+
+        //roll method
+        public int roll()
+        {
+            //min 1, 6
+            numberRolled = numberGenerator.Next(1, 7);
+            return numberRolled;
+        }
+
+        public int numberLastRolled()
+        {
+            return numberRolled;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
