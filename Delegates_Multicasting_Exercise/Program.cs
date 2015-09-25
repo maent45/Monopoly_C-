f67d148f538651802a8501ac57eea44da47b1935
Maen Terawasi <maent45@gmail.com>
2015-09-25 13:59:58 +1200

    added multicasting sample
    currently working on multicasting, some issues still need resolving

---
+++ Delegates_Multicasting_Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Multitask
+{
+    class Program
+    {
+        public delegate int Calculate(int value1, int value2);
+        static void Main(string[] args)
+        {
+            myUtil myUtilObject = new myUtil();
+
+            Calculate calculateFunction = new Calculate(myUtilObject.addMethod);
+
+            calculateFunction = calculateFunction + new Calculate(myUtilObject.divideMethod);
+            calculateFunction = calculateFunction + new Calculate(myUtilObject.multiplyMethod);
+
+        }
+    }
+}
---
+++ Delegates_Multicasting_Exercise/myUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Multitask
+{
+    public class myUtil
+    {
+        public void addMethod(int num1, int num2)
+        {
+            Console.WriteLine("adding two values: " + (num1 + num2));
+        }
+        public void multiplyMethod(int num1, int num2)
+        {
+            Console.WriteLine("adding two values: " + (num1 * num2));
+        }
+        public void divideMethod(int num1, int num2)
+        {
+            Console.WriteLine("adding two values: " + (num1 / num2));
+        }
+
+    }
+}
