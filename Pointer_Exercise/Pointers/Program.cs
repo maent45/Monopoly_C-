229e381367a1749eb274b5c950e21f48fc8efecb
Maen Terawasi <maent45@gmail.com>
2015-09-25 13:29:50 +1200

    added pointers
    C# pointers

---
+++ Pointer_Exercise/Pointers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pointers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5, j = 99, k = 109;
+            //enable unsafe pointer
+            unsafe
+            {
+                //declare int pointer
+                int* myIntPointer;
+
+                //direct the pointer to i address
+                myIntPointer = &i;
+                Console.WriteLine("memory address for var i is: " + (int)myIntPointer);
+                Console.WriteLine("value for var i address is: " + *myIntPointer);
+
+                Console.Read();
+
+                //direct the pointer to i address
+                myIntPointer = &i;
+                Console.WriteLine("memory address for var i is: " + (int)myIntPointer);
+                Console.WriteLine("value for var i address is: " + *myIntPointer);
+
+                Console.Read();
+            }
+        }
+    }
+}
