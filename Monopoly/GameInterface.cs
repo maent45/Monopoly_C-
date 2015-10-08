7c5db9b7714ef57d2906309f742d7717d5cb0355
Maen Terawasi <maent45@gmail.com>
2015-10-08 15:37:27 +1300

    added gameInterface
    started on gameInterface, and PropertyFactory class

---
+++ Monopoly/GameInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    //this class provides Interface for games
+    interface GameInterface
+    {
+        void initializeGame();
+        void makePlay(int player);
+        bool endOfGame();
+        void printWinner();
+        void playOneGame();
+    }
+}
---
+++ Monopoly/PropertyFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly
+{
+    //this class represents factory method which other factories will inherit from
+
+    //declare class as public
+    public class PropertyFactory
+    {
+        public Property create(string name)
+        {
+
+        }
+    }
+}
