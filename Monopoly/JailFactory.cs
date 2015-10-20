9acf1434c4f2c264535fd82e176f5dd2b433ca24
Maen Terawasi <maent45@gmail.com>
2015-10-20 20:57:52 +1300

    started jail factory
    began creating the jail factory

---
+++ Monopoly/JailFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MolopolyGame
+{
+    //JailFactory class extends from PropertyFactory class
+    public class JailFactory : PropertyFactory
+    {
+        public Jail create(string sName, bool isJail)
+        {
+            return new Jail(sName, isJail);
+        }
+
+    }
+}
