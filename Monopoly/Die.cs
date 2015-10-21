43e1b7b0d528bd57350b27e86e77b9438f5f23c6
Maen Terawasi <maent45@gmail.com>
2015-10-22 02:20:54 +1300

    updating Die class
    small changes to Die class

--- Monopoly/Die.cs
+++ Monopoly/Die.cs
@@ -1,33 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Monopoly
+namespace MolopolyGame
 {
-    //This is a die class that will generate random numbers between 1-6 which represents a dice roll.
-    class Die
+     /// <summary>
+    /// This is class for a die that generates random number 1-6 inclusive
+    /// </summary>
+    
+    public class Die
     {
-        //create new instance of Randon
-        private static Random numberGenerator = new Random();
+        //create new instance of Player
+        //Player player = new Player();
+        private static Random numGenerator = new Random();
+        
         private int numberRolled;
-
-        //roll method
+        
         public int roll()
         {
-            //min 1, 6
-            numberRolled = numberGenerator.Next(1, 7);
+            numberRolled = numGenerator.Next(1, 7);
             return numberRolled;
+            //return 30;
         }
 
         public int numberLastRolled()
         {
             return numberRolled;
         }
+         
         public override string ToString()
         {
-            return base.ToString();
+            return numberRolled.ToString();
         }
     }
 }
