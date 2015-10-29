904661628bbd313ded7ecfddc2bc8b4654e2835f
Maen Terawasi <maent45@gmail.com>
2015-10-29 18:42:10 +1300

    moved mortgage methods
    commented out mortgage methods in property class to move into
    tradeableProperty class

--- Monopoly/Property.cs
+++ Monopoly/Property.cs
@@ -13,7 +13,6 @@ namespace MolopolyGame
         protected string sName;
         protected Trader owner;
         protected bool bMortgaged;
-        protected bool bUnMortgaged;
         protected decimal dPrice;
         //protected decimal mortgageRate;
                
@@ -66,85 +65,90 @@ namespace MolopolyGame
             return false;//generic properties are not available for purchase
         }
 
-        /*------ METHODS TO MORTGAGE AND UNMORTGAGE PROPERTIES ------*/
-        //REFERENCE -> snippets of the following methods were obtained from Luke Hardiman
-        //is the property mortgaged?
-        public virtual bool isMortgaged()
-        {
-            return this.bMortgaged;
-        }
+        ///*------ METHODS TO MORTGAGE AND UNMORTGAGE PROPERTIES ------*/
+        ////REFERENCE -> snippets of the following methods were obtained from Luke Hardiman
+        ////is the property mortgaged?
+        //public virtual bool getMortgagedStatus()
+        //{
+        //    return this.bMortgaged;
+        //}
 
-        //calculate the mortage value
-        public virtual decimal calculateMortgage(Property property)
-        {
-            decimal dMortgagePrice = 0;
-            //Get types of properties
-            //REFERENCE -> getting property type code retrieved from https://msdn.microsoft.com/en-us/library/58918ffs.aspx
-            System.Type residential = typeof(Residential);
-            System.Type utility = typeof(Utility);
-            System.Type transport = typeof(Transport);
+        ////calculate the mortage value
+        //public virtual decimal calculateMortgage(Property property)
+        //{
+        //    decimal dMortgagePrice = 0;
+        //    //Get types of properties
+        //    //REFERENCE -> getting property type code retrieved from https://msdn.microsoft.com/en-us/library/58918ffs.aspx
+        //    System.Type residential = typeof(Residential);
+        //    System.Type utility = typeof(Utility);
+        //    System.Type transport = typeof(Transport);
 
-            if (property.GetType() == residential)
-            {
-                //cast the property as Residential
-                Residential residentialProperty = (Residential)property;
-                dMortgagePrice = residentialProperty.getPrice();
-            }
-            else if (property.GetType() == utility)
-            {
-                //cast the property as Utility
-                Utility utilityProperty = (Utility)property;
-                dMortgagePrice = utilityProperty.getPrice();
-            }
-            else if (property.GetType() == transport)
-            {
-                //cast the property as Transport
-                Transport transportProperty = (Transport)property;
-                dMortgagePrice = transportProperty.getPrice();
-            }
+        //    if (property.GetType() == residential)
+        //    {
+        //        //cast the property as Residential
+        //        Residential residentialProperty = (Residential)property;
+        //        dMortgagePrice = residentialProperty.getPrice();
+        //    }
+        //    else if (property.GetType() == utility)
+        //    {
+        //        //cast the property as Utility
+        //        Utility utilityProperty = (Utility)property;
+        //        dMortgagePrice = utilityProperty.getPrice();
+        //    }
+        //    else if (property.GetType() == transport)
+        //    {
+        //        //cast the property as Transport
+        //        Transport transportProperty = (Transport)property;
+        //        dMortgagePrice = transportProperty.getPrice();
+        //    }
 
-            return dMortgagePrice * 80 / 100;
+        //    return dMortgagePrice * 80 / 100;
 
-        }
+        //}
 
-        //logic for mortgaging propoety, add checks then proceed with mortgage
-        public virtual void mortgageProperty()
-        {
-            this.bMortgaged = true;
-        }
+        ////logic for mortgaging propoety, add checks then proceed with mortgage
+        //public virtual void setPropertyIsMortgaged()
+        //{
+        //    this.bMortgaged = true;
+        //}
 
-        /*----- METHODS TO UNMORTGAGE -----*/
-        //calculate 10% of property price as the unmortgaging rate
-        public virtual decimal calculateUnMortgage(Property property)
-        {
-            return this.dPrice * 10 / 100 + calculateMortgage(property);
-        }
+        //public void setPropertyNotMortgaged()
+        //{
+        //    this.bMortgaged = false;
+        //}
 
-        //pay off the property mortgage
-        public virtual void unMortgage(Property property)
-        {
-            //check if player has enough money in balance to pay off mortgage
-            if (this.getOwner().getBalance() <= (this.calculateUnMortgage(property)))
-            {
-                Console.WriteLine("Sorry, you don't have enough moneys to pay off da mortgage!");
-            }
-            else
-            {
-                //if player can afford, then call payOffMortgage()
-                payOffMortgage(property);
-            }
-        }
+        ///*----- METHODS TO UNMORTGAGE -----*/
+        ////calculate 10% of property price as the unmortgaging rate
+        //public virtual decimal calculateUnMortgage(Property property)
+        //{
+        //    return this.dPrice * 10 / 100 + calculateMortgage(property);
+        //}
 
-        //method to pay unMortgage of property
-        private void payOffMortgage(Property property)
-        {
-            //get owner of this property
-            this.getOwner().pay(calculateUnMortgage(property));
-            //bank then receives payment
-            Banker.access().receive(calculateUnMortgage(property));
-            //then set isMortgaged to false
-            this.bMortgaged = false;
-        }
+        ////pay off the property mortgage
+        //public virtual void unMortgage(Property property)
+        //{
+        //    //check if player has enough money in balance to pay off mortgage
+        //    if (this.getOwner().getBalance() <= (this.calculateUnMortgage(property)))
+        //    {
+        //        Console.WriteLine("Sorry, you don't have enough moneys to pay off da mortgage!");
+        //    }
+        //    else
+        //    {
+        //        //if player can afford, then call payOffMortgage()
+        //        payOffMortgage(property);
+        //    }
+        //}
+
+        ////method to pay unMortgage of property
+        //private void payOffMortgage(Property property)
+        //{
+        //    //get owner of this property
+        //    this.getOwner().pay(calculateUnMortgage(property));
+        //    //bank then receives payment
+        //    Banker.access().receive(calculateUnMortgage(property));
+        //    //then set isMortgaged to false
+        //    this.bMortgaged = false;
+        //}
     }
    
 }
