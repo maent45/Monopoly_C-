91d736dc4124af84e4556fd1ce85c20661491a7a
Maen Terawasi <maent45@gmail.com>
2015-11-02 01:48:08 +1300

    pushing community chest factory

---
+++ Monopoly/CommunityChestFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MolopolyGame
+{
+    public class CommunityChestFactory : PropertyFactory
+    {
+
+        public Community_Chest create(string sName)
+        {
+            return new Community_Chest(sName);
+        }
+
+    }
+
+    //public class Chance_Factory : PropertyFactory
+    //{
+
+    //    public Chance_Cards create(string sName)
+    //    {
+    //        return new Chance_Cards(sName);
+    //    }
+
+    //}
+}
