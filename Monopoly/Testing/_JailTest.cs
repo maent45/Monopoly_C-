8fae1ec7c9c252c822d0282660bddcd4f674b592
Maen Terawasi <maent45@gmail.com>
2015-11-02 21:23:08 +1300

    jail tests

---
+++ Monopoly/Testing/_JailTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MolopolyGame.Testing
+{
+    [TestFixture]
+    public class _JailTest
+    {
+        //create global instances to use to test with
+        private Jail theTestPlayer = new Jail();
+
+        [Test]
+        public void test_isJailProperty()
+        {
+            theTestPlayer.isJailProperty();
+            Assert.NotNull(theTestPlayer);
+        }
+
+        [Test]
+        public void test_ToString()
+        {
+            theTestPlayer.ToString();
+            Assert.NotNull(theTestPlayer);
+        }
+    }
+}
