07ce2cfafd34fab202fb02af79eada0789fb3053
Maen Terawasi <maent45@gmail.com>
2015-11-02 21:23:51 +1300

    jailFact tests

---
+++ Monopoly/Testing/_JailFactoryTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MolopolyGame.Testing
+{
+    [TestFixture]
+    public class _JailFactoryTest
+    {
+        //global instances for testing
+        JailFactory jFact = new JailFactory();
+
+        [Test]
+        //simple test for create method
+        public void test_create()
+        {
+            jFact.create("Jail", true);
+            Assert.NotNull(jFact);
+        }
+    }
+}
